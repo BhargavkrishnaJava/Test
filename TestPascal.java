5e7fcb8b95a6c8925ac51314252fa966dfd68bef
BhargavkrishnaJava <bhargavkrishna353@gmail.com>
2017-02-19 12:15:39 -0600

    Pascal Code Challenge

--- TestPascal.java
+++ TestPascal.java
@@ -2,22 +2,28 @@ import java.util.Scanner;
 
 public class TestPascal {
 
-	public static void main(String[] args) {
-		int rows, i, k, count = 1, j;
-		Scanner scan = new Scanner(System.in);
-		System.out.print("Enter Number of Rows : ");
-		rows = scan.nextInt();
-		for (i = 0; i < rows; i++) {
-			for (k = rows; k > i; k--) {
+	public static void triangle(int rows)
+	{
+		for (int i = 0; i < rows; i++) {
+			for (int k = rows; k > i; k--) {
 				System.out.print(" ");
 			}
-			count = 1;
-			for (j = 0; j <= i; j++) {
+			int count = 1;
+			for (int j = 0; j <= i; j++) {
 				System.out.print(count + " ");
 				count = count * (i - j) / (j + 1);
 			}
 			System.out.println();
 		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//int rows, i, k, count = 1, j;
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter Number of Rows : ");
+		int rows = scan.nextInt();
+		triangle(rows);
+		
 	}
 
 }
